4227f7215fa425f2bf41ffccdfc6b8bd64f00923
Ewerton Vidoy <ewertonvidoy@gmail.com>
2025-02-17 11:37:44 -0400

    alteração do arquivo feito por ewerton

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ print("é apenas mais uma modificação")
 print ("uma alteração está sendo feita")
 print ("uma alteração é necessário para este código")
 print("essa alteração foi feita por ewerton")
+if true:
+    print("mostre essa mensagem")
+else:
+    print("mostre essa outra mensagem")
